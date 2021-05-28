c3a0fe237b814b468e951931c2e8358e5ba27218
LEEHYUNDONG <l.steve10@yahoo.com>
2021-05-28 16:31:28 +0900

    add comment line by line

--- back/data/models.py
+++ back/data/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
+# 모델
 class Data(models.Model):
-    id = models.CharField(max_length=20, primary_key=True,default='B')
-    image = models.ImageField(upload_to='face', default='../media/james.jpg')
+    id = models.CharField(max_length=20, primary_key=True,default='B') # ID는 CHAR 필드를 가진다.
+    image = models.ImageField(upload_to='face', default='../media/james.jpg') # IMAGE를 저장시킬 변수
 
 
-    def __str__(self):
+    def __str__(self):  # DATA를 CALL할 때 그것의 ID값을 화면에 문자열로 출력시키기 위한 함수
         return self.id
