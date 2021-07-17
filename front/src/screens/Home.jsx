f4295f995e7f6b8d80eeb5443322a794651000a2
zzho-o <rkdwjsgh0@naver.com>
2021-07-17 18:32:58 +0900

    Home modif

--- front/src/screens/Home.jsx
+++ front/src/screens/Home.jsx
@@ -2,6 +2,10 @@ import React, { Component, useLayoutEffect } from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet, View } from "react-native";
 import { ImageBackground } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { MaterialIcons } from "@expo/vector-icons";
+
+
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -25,18 +29,31 @@ const styles = StyleSheet.create({
   }
 });
 
+const _handleqrpress = () => {
+
+}
+
 //<Text style={{ fontSize: 24 }}>Home</Text>
 const Home=({}) => {
   
   return (
+    <KeyboardAwareScrollView
+          extraHeight={20}>
     <ImageBackground
-      style={{ width: "100%", height: "100%" }}
+      style={{ width: "100%", height: "200%" }}
       source={require("../../assets/backgroundIMG/loading.png")}
       resizeMode="cover">
-      <View>
-      <Text style={{ fontSize: 24 }}>Home</Text>
+      <View style={{alignItems:"flex-end"}}>
+      <MaterialIcons
+  name="qr-code"
+  size={30}
+  style={{ margin: 10 }}
+  //onpress={{}}
+/>
       </View>
     </ImageBackground>
+        </KeyboardAwareScrollView>
+
     );
   
 };
