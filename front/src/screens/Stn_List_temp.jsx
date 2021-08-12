e76c6b015d4218b53ae68d5843ca1240396b7272
zzho-o <>
2021-08-12 16:40:18 +0900

    changing attendence

--- front/src/screens/Stn_List_temp.jsx
+++ front/src/screens/Stn_List_temp.jsx
@@ -14,28 +14,30 @@ const Stn_List_temp = ({ route }) => {
     let len = Object.keys(route.params.a_week).length;
     let obj = route.params.a_week;
     const _handlePressBotton = params => {
-        console.log('params:'+params.late,params.check,params.absent)
         if (params.check == 'attd'){
-            console.log('check!')
+            console.log(params.id+1+'교시'+'check!')
             obj[params.id]['attd'] = 'check';
         }
         else if (params.late == 'late'){
-            console.log('late!')
+            console.log(params.id+1+'교시'+'late!')
             obj[params.id]['attd'] = 'late';
         }
         else if (params.absent == 'absent'){
-            console.log('absent!')
+            console.log(params.id+1+'교시'+'absent!')
             obj[params.id]['attd'] = 'abcent';
         }
         
     }
+    
+    useEffect(() => {
+        console.log(123)
+    },[obj[0]['attd'],obj[1]['attd'],obj[2]['attd']]);
 
     return (
         <Container>
             <Student_edit id={0} attd={obj[0]['attd']} onPress={_handlePressBotton} />
             <Student_edit id={1} attd={obj[1]['attd']} onPress={_handlePressBotton}/>
             {len == 2 ? null : <Student_edit id={2} attd={obj[2]['attd']} onPress={_handlePressBotton}/>}
-            <Text>{console.log(route.params.a_week)}</Text>
         </Container>
     );
 };
