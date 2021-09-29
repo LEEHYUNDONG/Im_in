69f2adf36c7a1c6a24c479ab2fc1d9e208797961
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-29 17:31:29 +0900

    init BE

--- back/data/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Data
-
-# Register your models here.
-admin.site.register(Data) # 관리자 권한으로 DATA URL에 접속할 수 있게 함.
--- back/myProfile/urls.py
+++ back/myProfile/urls.py
@@ -1,7 +1,7 @@
-"""restAPI URL Configuration
+"""myProfile URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-router = routers.DefaultRouter() # ROUTER를 사용하여 RESTAPI를 찾아가게 했었음. 현재 미사용.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('data.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #MEDIA 값을 SETTINGS에 추가 시켜놨기 때문에 SETTINGS에 있는 URL을 위 PATTERN에 더하게 되면 MEDIA값에 ACCESS 가능.
+]
