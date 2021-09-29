030a7ad6f3ed2e5c0fdf5bf93357df08748a851c
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-29 18:27:07 +0900

    model

--- back/checkImage/models.py
+++ back/checkImage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import os
 
-# Create your models here.
+
+def image_upload_path(instance, filename):
+    return 'check/'+os.path.join(instance.title, filename)
+
+
+class checkImage(models.Model):
+
+    title = models.CharField(max_length=200, default='None')
+    image = models.ImageField(
+        upload_to=image_upload_path, default='../media/james.jpg')
+
+    def __str__(self):
+        return self.title
